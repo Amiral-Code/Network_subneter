f46718cc4ef202d80e2a29bad7a5bdd39ddfbaf9
Amiral Code <seddoum37@gmail.com>
2024-06-15 16:58:36 +0100

    Update

--- Network_SubneterV2.py
+++ Network_SubneterV2.py
@@ -5,7 +5,7 @@ def get_address():
     while True:
         address = input("Enter the network address: ")
         try:
-            network = ipaddress.IPv4Network(address)
+            network = ipaddress.IPv4Network(address, strict=False)
             print('Network address:', network)
             return network
         except ValueError:
@@ -13,161 +13,126 @@ def get_address():
 
 def get_mode():
     while True:
-        mode = input("What mode do you want to use to segment this network (VLSM or FLSM): ").lower()
-        if mode.startswith('v'):
-            print('The mode you chose is VLSM.')
-            return 'VLSM'
-        elif mode.lower().startswith('f'):
-            print('The mode you chose is FLSM.')
-            return 'FLSM'
+        mode = input("What mode do you want to use to segment this network (VLSM or FLSM): ").strip().upper()
+        if mode in ['VLSM', 'FLSM']:
+            print(f'The mode you chose is {mode}.')
+            return mode
         else:
-            print('Please, enter a valid mode.')
+            print('Please, enter a valid mode (VLSM or FLSM).')
 
 def get_subnet_num():
     while True:
         subnet_num = input('How many subnets are in this network: ')
-        if subnet_num.isdigit():
+        if subnet_num.isdigit() and int(subnet_num) > 0:
             print('The number of subnets in this network is:', subnet_num)
             return int(subnet_num)
         else:
-            print('Please enter a valid subnets number')
+            print('Please enter a valid number of subnets (greater than 0).')
 
-def get_subnet_host_num_vlsm(subnet_num):
-    subnet_host_num = []
+def get_subnet_host_counts_vlsm(subnet_num):
+    subnet_host_counts = []
     for i in range(subnet_num):
         while True:
             host_num = input(f'How many hosts are in Subnet {i + 1}: ')
             if host_num.isdigit() and int(host_num) > 0:
-                subnet_host_num.append(int(host_num))
+                subnet_host_counts.append(int(host_num))
                 break
             else:
                 print('Please enter a valid number of hosts (greater than 0).')
-    subnet_host_num.sort(reverse=True)
-    print('The number of hosts in each subnet are:', subnet_host_num)
-    return subnet_host_num
+    subnet_host_counts.sort(reverse=True)
+    print('The number of hosts in each subnet are:', subnet_host_counts)
+    return subnet_host_counts
 
-def get_subnet_host_num_flsm(subnet_num):
-    subnet_host_num = []
-    max_host_num = 0  
+def get_subnet_host_counts_flsm(subnet_num):
+    max_host_num = 0
     for i in range(subnet_num):
         while True:
             try:
                 host_num = int(input(f'How many hosts are in Subnet {i + 1}: '))
                 if host_num > 0:
-                    if host_num > max_host_num:
-                        max_host_num = host_num  # Update max_host_num if a higher value is entered
+                    max_host_num = max(max_host_num, host_num)
                     break
                 else:
                     print('Please enter a valid number of hosts (greater than 0).')
             except ValueError:
                 print('Please enter a valid integer.')
-
-    subnet_host_num = [max_host_num] * subnet_num
+    subnet_host_counts = [max_host_num] * subnet_num
     print('The highest number of hosts entered in any subnet is:', max_host_num)
-    print('The list of hosts in each subnet is:', subnet_host_num)
-    return subnet_host_num
+    return subnet_host_counts
 
 def get_mask(network):
-    mask = network.prefixlen
-    return mask
+    return network.prefixlen
 
 def get_ip_address(network):
-    ip_address = network.network_address
-    return ip_address
+    return network.network_address
 
-def get_subnet_host_bits(subnet_host_num):
-    subnet_host_bits = [int(math.ceil(math.log2(i + 2))) for i in subnet_host_num]
+def get_subnet_host_bits(subnet_host_counts):
+    subnet_host_bits = [int(math.ceil(math.log2(count + 2))) for count in subnet_host_counts]
     print('The host bits of subnets are:', subnet_host_bits)
     return subnet_host_bits
 
 def get_subnets_mask(mask, subnet_host_bits):
-    subnets_mask = []
-    for i in subnet_host_bits:
-        subnet_mask = mask + ((32 - mask) - i)
-        subnets_mask.append(subnet_mask)
+    subnets_mask = [mask + ((32 - mask) - bits) for bits in subnet_host_bits]
     print('The subnets masks for this network are:', subnets_mask)
     return subnets_mask
 
 def get_subnets(network, subnets_mask):
     subnets = []
-    ip_address = get_ip_address(network)
-    address = ip_address
-    mask = get_mask(network)
-
-    for i in subnets_mask:
-        subnet = ipaddress.ip_network(str(address) + '/' + str(i))
+    address = get_ip_address(network)
+    for mask in subnets_mask:
+        subnet = ipaddress.ip_network(f'{address}/{mask}', strict=False)
         subnets.append(subnet)
         address = subnet.network_address + subnet.num_addresses
     return subnets
 
 def get_subnet_info(subnets):
-    subnets_address = []
-    subnets_mask = []
-    first_address_ip = []
-    last_address_ip = []
-    broadcast_address = []
-    hosts_num = []
     subnets_info = []
-    for i in subnets:
-        subnets_address.append(i)
-        subnets_mask.append(i.netmask)
-        subnet = i.hosts
-        first_address_ip.append(i.network_address + 1)
-        last_address_ip.append(i.broadcast_address - 1)
-        broadcast_address.append(i.broadcast_address)
-        hosts_num.append(i.num_addresses - 2)
-    
-    for j in range(len(subnets)):
-        subnets_info.append([subnets_address[j],
-        subnets_mask[j],
-        first_address_ip[j],
-        last_address_ip[j],
-        broadcast_address[j],
-        hosts_num[j]])
-    print()
+    for subnet in subnets:
+        info = {
+            'network_address': subnet.network_address,
+            'netmask': subnet.netmask,
+            'first_host': subnet.network_address + 1,
+            'last_host': subnet.broadcast_address - 1,
+            'broadcast_address': subnet.broadcast_address,
+            'host_count': subnet.num_addresses - 2
+        }
+        subnets_info.append(info)
     return subnets_info
 
-def get_subnet_data(subnets_info):
-    x = 1
-    for i in subnets_info:
-        print(f'subnet{x} address is:', i[0])
-        print(f'subnet{x} mask address is:', i[1])
-        print(f'subnet{x} first host address is:', i[2])
-        print(f'subnet{x} last host address is:', i[3])
-        print(f'subnet{x} broadcast address is:', i[4])
-        print(f'subnet{x} host number is:', i[5])
+def display_subnet_info(subnets_info):
+    for idx, info in enumerate(subnets_info, start=1):
+        print(f'Subnet {idx} Information:')
+        print(f'  Network Address: {info["network_address"]}')
+        print(f'  Netmask: {info["netmask"]}')
+        print(f'  First Host: {info["first_host"]}')
+        print(f'  Last Host: {info["last_host"]}')
+        print(f'  Broadcast Address: {info["broadcast_address"]}')
+        print(f'  Number of Hosts: {info["host_count"]}')
         print()
-        x += 1
 
 def main():
     while True:
         network = get_address()
         mask = get_mask(network)
         mode = get_mode()
+        
         if mode == 'VLSM':
-            subnet_num_vlsm = get_subnet_num()
-            subnet_host_num_vlsm = get_subnet_host_num_vlsm(subnet_num_vlsm)
-            subnet_host_bits = get_subnet_host_bits(subnet_host_num_vlsm)
-            subnets_mask = get_subnets_mask(mask, subnet_host_bits)
-            subnets = get_subnets(network, subnets_mask)
-            subnets_info = get_subnet_info(subnets)
-            subnets_data = get_subnet_data(subnets_info)
-            print('Do you want to subnet an other network? (yes or no)')
-            if not input('> ').lower().startswith('y'):
-                print('Thanks for using our script!')
-                break
+            subnet_num = get_subnet_num()
+            subnet_host_counts = get_subnet_host_counts_vlsm(subnet_num)
         else:
-            subnet_host_num_flsm = get_subnet_num()
-            subnet_host_num_flsm = get_subnet_host_num_flsm(subnet_host_num_flsm)
-            subnet_host_bits = get_subnet_host_bits(subnet_host_num_flsm)
-            subnets_mask = get_subnets_mask(mask, subnet_host_bits)
-            subnets = get_subnets(network, subnets_mask)
-            subnets_info = get_subnet_info(subnets)
-            subnets_data = get_subnet_data(subnets_info)
-            print('Do you want to subnet an other network? (yes or no)')
-            if not input('> ').lower().startswith('y'):
-                print('Thanks for using our script!')
-                break
+            subnet_num = get_subnet_num()
+            subnet_host_counts = get_subnet_host_counts_flsm(subnet_num)
+        
+        subnet_host_bits = get_subnet_host_bits(subnet_host_counts)
+        subnets_mask = get_subnets_mask(mask, subnet_host_bits)
+        subnets = get_subnets(network, subnets_mask)
+        subnets_info = get_subnet_info(subnets)
+        display_subnet_info(subnets_info)
+        
+        cont = input('Do you want to subnet another network? (yes or no): ').strip().lower()
+        if not cont.startswith('y'):
+            print('Thanks for using our script!')
+            break
 
 if __name__ == "__main__":
     main()
